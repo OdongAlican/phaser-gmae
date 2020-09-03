7eeafa8b7fcd261de4be4eee89f58ee43787c3f6
OdongAlican <sandieo.2020@gmail.com>
2020-09-04 01:27:20 +0300

    test for posting user details

---
+++ test/postScore.test.js
@@ -0,0 +1,11 @@
+import 'babel-polyfill';
+import { saveScores } from './mocks/mocker'
+
+test('Check for correct data posted to the provided URL', async ()=>{
+    const result = await saveScores();
+
+    expect(result.user).toBe('TestUserTwo')
+    expect(result.user).not.toBe('TestUser')
+    expect(typeof saveScores()).toBe('object');
+    expect(typeof saveScores()).not.toBe('string');
+})
