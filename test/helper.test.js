ac106765847feb735e5037a92c8aa596a26d0a17
OdongAlican <sandieo.2020@gmail.com>
2020-09-04 18:32:16 +0300

    add tests to the project

--- test/helper.test.js
+++ test/helper.test.js
@@ -8,3 +8,15 @@ test('update score', () => {
   expect(window.score).toBe(20);
   expect(window.score).not.toBe(10);
 });
+
+test('checks the type of data as the score', () => {
+  Helper.updateScore('MainScene', 320);
+  expect(typeof window.score).toBe('number');
+  expect(typeof window.score).not.toBe('object');
+  expect(typeof window.score).not.toBe('string');
+});
+
+test('checks that there is always a score value', () => {
+  Helper.updateScore('MainScene', 120);
+  expect(window.score).not.toBe('');
+});
